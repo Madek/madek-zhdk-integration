5ff070fac21ff4f6ad3b5a046e33766fe5a3ca0f
Thomas Schank <DrTom@schank.ch>
2014-09-08 14:11:34 +0200

    Add zhdk_ldap_mapping

---
+++ app/lib/madek_zhdk_integration/ldap_mapper.rb
@@ -0,0 +1,27 @@
+module MadekZhdkIntegration
+  module LdapMapper
+
+    class << self
+
+      def reload! 
+        load Rails.root.join(__FILE__)
+      end
+
+      def module_path # for convenient reloading
+        Rails.root.join(__FILE__)
+      end
+
+
+      def create_map 
+        File.open(Rails.root.join("tmp","ldap_map.yml"),"w") do |file|
+          file.write(
+            Hash[
+              InstitutionalGroup.all.map do |g| 
+                [g.ldap_name,g.name.gsub(/\n/, " ").strip]
+              end.sort].to_yaml(line_width: -1))
+        end
+      end
+
+    end
+  end
+end
