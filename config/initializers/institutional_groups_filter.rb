c0c7ebd6c269cd8966613c48eef1f30682931f6c
Max F. Albrecht <1@178.is>
2014-10-31 13:27:46 +0100

    InstitutionalGroups: .selectable: correct filtering

--- config/initializers/institutional_groups_filter.rb
+++ config/initializers/institutional_groups_filter.rb
@@ -1,16 +1,14 @@
 InstitutionalGroup.class_eval do
 
-  if Settings.zhdk_integration 
+  if Settings.zhdk_integration
 
     scope :selectable, lambda{
-      where("institutional_group_name NOT SIMILAR TO '%_[0-9]{2}[A-Za-z]\.studierende'")
-      .where("institutional_group_name NOT SIMILAR TO 'Verteilerliste\.%'")
-      .where("institutional_group_name NOT SIMILAR TO 'Personal\.%'")
-      .where("institutional_group_name !~* '^dozierende'")
-      .where("institutional_group_name !~* '^mittelbau'")
-      .where("institutional_group_name !~* '^personal'")
-      .where("institutional_group_name !~* '^studirende'")
-      .where("institutional_group_name !~* '^verteilerliste'")
+      where("institutional_group_name ~* '\.alle$'")
+      .where("institutional_group_name !~* '^[Dd]ozierende\.'")
+      .where("institutional_group_name !~* '^[Mm]ittelbau\.'")
+      .where("institutional_group_name !~* '^[Pp]ersonal\.'")
+      .where("institutional_group_name !~* '^[Ss]tudierende\.'")
+      .where("institutional_group_name !~* '^[Vv]erteilerliste\.'")
     }
 
   end
