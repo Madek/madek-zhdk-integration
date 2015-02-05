3554bd7ee822ff1cc80e62941594e4db12b8831d
Matus Kmit <matus.kmit@zhdk.ch>
2015-02-05 12:43:14 +0100

    make routes available in host application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-MadekZhdkIntegration::Engine.routes.draw do
+Rails.application.routes.draw do
 
   get '/login', :to => "madek_zhdk_integration/authentication#login"
   get '/authenticator/zhdk/login_successful/:id', :to => "madek_zhdk_integration/authentication#login_successful"
