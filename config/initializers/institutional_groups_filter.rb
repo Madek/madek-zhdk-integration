d026d5530d797cee9154da897e5b44fbe56a2b25
Matus Kmit <matuskmit1@gmail.com>
2023-05-09 15:46:55 +0530

    fix depr warnings rails 7.0

--- config/initializers/institutional_groups_filter.rb
+++ config/initializers/institutional_groups_filter.rb
@@ -1,26 +1,28 @@
-InstitutionalGroup.class_eval do
+Rails.application.reloader.to_prepare do
+  InstitutionalGroup.class_eval do
 
-  def self.filter_sql
-    <<-SQL
-      institutional_name ~* '\.alle$'
-      AND institutional_name !~* '^[Dd]ozierende\.'
-      AND institutional_name !~* '^[Mm]ittelbau\.'
-      AND institutional_name !~* '^[Pp]ersonal\.'
-      AND institutional_name !~* '^[Ss]tudierende\.'
-      AND institutional_name !~* '^[Vv]erteilerliste\.'
-    SQL
-  end
+    def self.filter_sql
+      <<-SQL
+        institutional_name ~* '\.alle$'
+        AND institutional_name !~* '^[Dd]ozierende\.'
+        AND institutional_name !~* '^[Mm]ittelbau\.'
+        AND institutional_name !~* '^[Pp]ersonal\.'
+        AND institutional_name !~* '^[Ss]tudierende\.'
+        AND institutional_name !~* '^[Vv]erteilerliste\.'
+      SQL
+    end
 
-  if Settings.zhdk_integration
+    if Settings.zhdk_integration
 
-    scope :selectable, lambda{
-      where(filter_sql)
-    }
+      scope :selectable, lambda{
+        where(filter_sql)
+      }
 
-    def to_s
-      "#{name} (#{institutional_name})"
+      def to_s
+        "#{name} (#{institutional_name})"
+      end
+
     end
 
   end
-
 end
--- config/initializers/zhdk_group_filter.rb
+++ config/initializers/zhdk_group_filter.rb
@@ -1,23 +1,25 @@
-Group.class_eval do
+Rails.application.reloader.to_prepare do
+  Group.class_eval do
 
-  if Settings.zhdk_integration
+    if Settings.zhdk_integration
 
-    scope :filter_by, lambda{ |search_term, filter = nil, query_scope = nil|
-      query = default_query(search_term, filter)
-      zhdk_query(query, search_term, filter, query_scope)
-    }
+      scope :filter_by, lambda{ |search_term, filter = nil, query_scope = nil|
+        query = default_query(search_term, filter)
+        zhdk_query(query, search_term, filter, query_scope)
+      }
 
-    private
+      private
 
-    def self.zhdk_query(query, _search_term, _filter, query_scope)
-      if query_scope == 'metadata'
-        sql = <<-SQL
-          ((type = 'InstitutionalGroup' AND #{InstitutionalGroup.filter_sql})
-            OR type ='Group')
-        SQL
-        query.where(sql)
-      else
-        query
+      def self.zhdk_query(query, _search_term, _filter, query_scope)
+        if query_scope == 'metadata'
+          sql = <<-SQL
+            ((type = 'InstitutionalGroup' AND #{InstitutionalGroup.filter_sql})
+              OR type ='Group')
+          SQL
+          query.where(sql)
+        else
+          query
+        end
       end
     end
   end
